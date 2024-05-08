62699e02de6f59a30ae8a9e24c482fbc039702f4
Kitso Mogale <kitsomogale19@gmail.com>
2024-05-08 09:47:01 +0200

    if-else approach

---
+++ StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
@@ -0,0 +1,46 @@
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+const newWindow = document.getElementById("critical-error")
+let answer = 0;
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+  console.log( dividend)
+  console.log( divider)
+  
+  if(!(dividend === '' || divider==='')){
+
+    answer = dividend / divider;
+  
+   
+    if(isNaN(answer)){
+      
+      newWindow.style.display = 'block';
+    }
+    else{
+
+      if(divider == 0){
+          result.innerHTML = '<p class="error-message">Division not performed. Invalid number provided. Try again</p>' ;
+          console.error('Invalid number provided');
+     }
+     else{
+    
+          if(answer>=1){
+             answer = Math.floor(answer);
+
+             result.innerText = answer;
+         }
+         else{
+             result.innerText = answer
+             }
+
+       }
+
+      }
+     
+    }
+  else{
+    result.innerHTML = `<p class="error-message"> Division not performed. Both values are required in inputs. Try again</p>`
+  }
+});
